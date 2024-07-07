7ad7b181d419d6e58d276ca05926b2d4c22ac871
Bryan Plant <bryanplant@gmail.com>
2024-07-06 22:04:46 -0700

    update cacheName

--- Build/Web/ServiceWorker.js
+++ Build/Web/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-tswift-lang-0.1.1";
+const cacheName = "DefaultCompany-tswift-lang-0.1.2";
 const contentToCache = [
   "Build/Web.loader.js",
   "Build/Web.framework.js.unityweb",
